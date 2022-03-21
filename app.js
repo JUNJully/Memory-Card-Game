929ece8e8bd8e662e92d1209d20888a5c9038792
JUNJully <ysuvorova93@gmail.com>
2022-03-21 15:42:32 +0700

    add cards on the board

--- app.js
+++ app.js
@@ -3,37 +3,81 @@ const lives = document.querySelector('.lives') //select title for append count o
 
 lives.lastChild.textContent = 7;
 
+let arr =   [{imgSrc:"./img/flower.jpg", name:"flower"},
+             {imgSrc:"./img/fractal.jpg", name:"fractal"},
+             {imgSrc:"./img/game.jpg", name:"game"},
+             {imgSrc:"./img/cat.jpg", name:"cat"},
+             {imgSrc:"./img/geccon.jpg", name:"geccon"},
+             {imgSrc:"./img/kimono.jpg", name:"kimono"},
+             {imgSrc:"./img/kofe.jpg", name:"kofe"},
+             {imgSrc:"./img/love.jpg", name:"love"},
+             {imgSrc:"./img/flower.jpg", name:"flower"},
+             {imgSrc:"./img/fractal.jpg", name:"fractal"},
+             {imgSrc:"./img/game.jpg", name:"game"},
+             {imgSrc:"./img/cat.jpg", name:"cat"},
+             {imgSrc:"./img/geccon.jpg", name:"geccon"},
+             {imgSrc:"./img/kimono.jpg", name:"kimono"},
+             {imgSrc:"./img/kofe.jpg", name:"kofe"},
+             {imgSrc:"./img/love.jpg", name:"love"},
+]
+
 class Card {
     constructor(imgSrc,name) {
-        this.imgSrc = imgSrc;
         this.name = name;
-    }
-    cardGenerate() {
         this.card = document.createElement('div');
         this.face = document.createElement('img');
         this.back = document.createElement('back');
         this.card.classList = 'card';
         this.face.classList = 'face';
         this.back.classList = 'back';
+        this.face.src = imgSrc;
+        section.appendChild(this.card);
+        this.card.appendChild(this.face);
+        this.card.appendChild(this.back);
+        this.card.setAttribute("name",name);
     }
+
 }
 
-class generateBoard {
+
+class Board {
     constructor(array){
         this.array = array;
     }
     randomize() {
-        this.array.sort(() => Math.random()-0.5);
-    }  
+      this.array.sort(() => Math.random()-0.5);
+      return this;
+
+    } 
     generate() {
-        this.array.map(item => new Card(item.src, item.name))
+      this.array.map(item => new Card(item.imgSrc,item.name));
+      return this;
     }
 }
 
-class Board {
-    constructor(cards) {
-        this.cards = cards;
+class Game {
+    constructor() {
+        this.board = document.querySelector('section');
+        this.clickHandler = this.clickHandler.bind(this);
+        this.clickCard();
+    }
+    clickCard() {
+        this.board.addEventListener('click',this.clickHandler);
+    }
+    clickHandler(e) {
+        e.target.classList.toggle("toggleCard");
+     
     }
 }
+
+
+let board = new Board(arr);
+board.randomize().generate();
+const game = new Game(board);
+
+
+
+
+
 
 
